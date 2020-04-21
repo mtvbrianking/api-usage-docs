16600fdeecc6cc84936c5b45159988bc4ce43970
mtvbrianking <mtvbrianking@gmail.com>
2020-04-21 15:50:18 +0300

    Require minimal.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,9 @@
 let mix = require('laravel-mix');
 
-mix.copyDirectory('node_modules/jquery', 'public/vendor/jquery');
+mix.copyDirectory('node_modules/jquery/dist', 'public/vendor/jquery/dist');
 
-mix.copyDirectory('node_modules/redoc', 'public/vendor/redoc');
+mix.copyDirectory('node_modules/redoc/bundles', 'public/vendor/redoc/bundles');
 
 mix.copyDirectory('node_modules/swagger-editor-dist', 'public/vendor/swagger-editor-dist');
 
 mix.copyDirectory('node_modules/swagger-ui-dist', 'public/vendor/swagger-ui-dist');
-
